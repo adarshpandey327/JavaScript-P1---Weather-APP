f3fdd6af645bd66219354bd01eea6a22cad4ff9e
adarshpandey327 <130994929+adarshpandey327@users.noreply.github.com>
2024-01-11 16:55:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -33,35 +33,35 @@ async function checkWeather(city){
     const condition = data.weather[0].main;
     
     if(condition=="Clear"){
-        document.querySelector(".weather img").src="images/clear.png";
+        document.querySelector(".weather img").src="clear.png";
         document.querySelector(".weather h3").innerHTML=condition + ", Ghum aao Bahar";
     }
     else if(condition=='Clouds'){
-        document.querySelector(".weather img").src="images/clouds.png";
+        document.querySelector(".weather img").src="clouds.png";
         document.querySelector(".weather h3").innerHTML=condition + ", Mast Mausam";
     }
     else if(condition=="Drizzle"){
-        document.querySelector(".weather img").src="images/drizzle.png";
+        document.querySelector(".weather img").src="drizzle.png";
         document.querySelector(".weather h3").innerHTML=condition + ", Phuhaaar baras ri";
     }
     else if(condition=="Humidity"){
-        document.querySelector(".weather img").src="images/humidity.png";
+        document.querySelector(".weather img").src="humidity.png";
         document.querySelector(".weather h3").innerHTML=condition + ", Bhot nami hai";
     }
     else if(condition=="Mist"){
-        document.querySelector(".weather img").src="images/mist.png";
+        document.querySelector(".weather img").src="mist.png";
         document.querySelector(".weather h3").innerHTML=condition + ", Kohraaa";
     }
     else if(condition=="Rain"){
-        document.querySelector(".weather img").src="images/rain.png";
+        document.querySelector(".weather img").src="rain.png";
         document.querySelector(".weather h3").innerHTML=condition + ", Mat jao , bheegoge";
     }
     else if(condition=="Snow"){
-        document.querySelector(".weather img").src="images/snow.png";
+        document.querySelector(".weather img").src="snow.png";
         document.querySelector(".weather h3").innerHTML=condition + ", Waao baraf!";
     }
     else{
-        document.querySelector(".weather img").src="images/wind.png";
+        document.querySelector(".weather img").src="wind.png";
         document.querySelector(".weather h3").innerHTML=condition + ", Tez Hawayein";
     }
     document.querySelector(".weather").style.display="block";
